1a1c11b379c38506f08ef3a4f82ab0f0d1881799
QuinnSong <ccf-midi@hotmail.com>
2017-02-04 16:38:16 -0500

    unused files cleanup

--- src/create a numpy image.py
+++
@@ -1,8 +0,0 @@
-np_array =  np.empty( (w,h), np.uint32)
-np_array.shape = h, w
-np_array[:,:] = 0xFF0000FF #self.colorToHex(color_new)
-self.cur_im = Image.frombuffer('RGBA', (w,h), np_array, 'raw', 'RGBA', 0, 1)
-
-def colorToHex (self, color):
-    r,g,b,a = color
-    return eval('0x' + hex(a)[2:].zfill(2) + hex(b)[2:].zfill(2) + hex(g)[2:].zfill(2) + hex(r)[2:].zfill(2))
